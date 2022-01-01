1529229e53950e2c9882ab7b377563b93ea0ddf4
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2022-01-01 17:37:09 +0530

    Create 10SearchInRotatedSortedArray.py

---
+++ 10SearchInRotatedSortedArray.py
@@ -0,0 +1,25 @@
+# Question: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+# Solution:
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left+right)//2
+            
+            if (nums[mid] < nums[0]) == (target < nums[0]):
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] > target:
+                    right = mid
+                else:
+                    return mid
+            elif target < nums[0]:
+                left = mid+1
+            else:
+                right = mid
+        return -1
+
+# Verdict:
+# Runtime: 68 ms, faster than 6.56% of Python3 online submissions for Search in Rotated Sorted Array.
+# Memory Usage: 14.6 MB, less than 55.90% of Python3 online submissions for Search in Rotated Sorted Array.
